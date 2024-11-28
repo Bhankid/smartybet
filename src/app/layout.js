6ccb57231d65a883ed9cca06c5fa176a7274561a
Bhankid <alfredfianyo50@gmail.com>
2024-11-28 16:36:12 +0000

    dependencies version

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,8 +27,8 @@ export default function RootLayout({ children }) {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css"
           integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ=="
-          crossorigin="anonymous"
-          referrerpolicy="no-referrer"
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
         />
       </head>
       <body
@@ -36,7 +36,6 @@ export default function RootLayout({ children }) {
       >
         <Nav />
         {children}
-       
       </body>
     </html>
   );
